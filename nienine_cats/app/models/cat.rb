2530a267b15ea48345420f4e24add466bcd64883
mrossant <matteorossant@gmail.com>
2021-09-17 12:15:50 -0400

    Brushing up Cat Models

---
+++ nienine_cats/app/models/cat.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+    COLORS = ['Red', 'Yellow', 'Brown', 'Black', 'Grey', 'White']
+    SEX = ['M', 'F', 'N', 'Q']
+
+    validates :birth_date, :name, :sex, :description, presence: true
+    validates :color, inclusion: { in: COLORS }
+    validates :sex, inclusion: { in: SEX }
+
+end
