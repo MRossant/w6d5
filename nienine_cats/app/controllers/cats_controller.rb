26036e270343a1ec9cc0d580449ff1746250ae04
mariavaghani <maria.vaghani@gmail.com>
2021-09-17 15:47:20 -0400

    start form, show page table

--- nienine_cats/app/controllers/cats_controller.rb
+++ nienine_cats/app/controllers/cats_controller.rb
@@ -9,6 +9,21 @@ class CatsController < ApplicationController
     render :index
   end
 
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render json: @cat.errors.full_messages, status: 422
+    end
+  end
+
   private
   def cat_params
     params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
