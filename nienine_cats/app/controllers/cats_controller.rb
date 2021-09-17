d6edfeb39bf444d8e7a8d229c1852f172b93830e
mrossant <matteorossant@gmail.com>
2021-09-17 15:14:04 -0400

    Finished Show

--- nienine_cats/app/controllers/cats_controller.rb
+++ nienine_cats/app/controllers/cats_controller.rb
@@ -1,9 +1,16 @@
 class CatsController < ApplicationController
   def show
+    @cat = Cat.find(params[:id])
+    render :show
   end
 
   def index
     @cats = Cat.all
     render :index
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end
 end
--- nienine_cats/db/seeds.rb
+++ nienine_cats/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Create Cat
+Cat.destroy_all
+cat_1 = Cat.create!(birth_date: Date.new(2015,8,7), color: 'Red', name: 'Sky', sex: 'F', description: 'Best Cat')
+cat_2 = Cat.create!(birth_date: Date.new(2010,8,7), color: 'Yellow', name: 'Furball', sex: 'M', description: 'Fat Cat')
+cat_3 = Cat.create!(birth_date: Date.new(2001,8,7), color: 'Brown', name: 'Jeff', sex: 'M', description: 'Wise Cat')
+cat_4 = Cat.create!(birth_date: Date.new(2009,8,7), color: 'Grey', name: 'Dio', sex: 'F', description: 'Holy Cat')
+cat_5 = Cat.create!(birth_date: Date.new(2012,8,7), color: 'Black', name: 'Mary', sex: 'F', description: 'Cat')
